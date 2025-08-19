f1fa781b84ed94da367f75e69a9d7e13ec0dba7d
Ruben-legend <canojimenezrubenalexander@gmail.com>
2025-08-19 13:18:52 -0400

    i remove unused code

--- src/main.rs
+++ src/main.rs
@@ -40,28 +40,17 @@ fn main() {
     let status_section = create_section("status", "v", status);
     
     let main_box = create_section("main", "h", vec![name_section, image_section, status_section]);
-    /*let mut main_box = String::new();
-    main_box.push_str("(box :class \"docker-container\" :orientation \"vertical\" :space-evenly \"false\" :spacing 5 \n");
-    main_box.push_str("  (box :orientation \"horizontal\" :space-evenly \"false\" :spacing 15 \n");
-    main_box.push_str(&format!("    (label :class \"docker-names\" :text \"Names\" :halign \"start\" :valign \"center\")\n"));
-    main_box.push_str(&format!("    (label :class \"docker-images\" :text \"Images\" :halign \"start\" :valign \"center\")\n"));
-    main_box.push_str(&format!("    (label :class \"docker-status\" :text \"Status\" :halign \"start\" :valign \"center\")\n"));
-    main_box.push_str("  )\n");
-    main_box.push_str(&name_section);
-    main_box.push_str(&image_section);
-    main_box.push_str(&status_section);
-    main_box.push_str(")\n");   */
     println!("{}", main_box);
 }
 
 fn create_section(name: &str, orinentation: &str, list: Vec<String>) -> String{
     let mut main_box = String::new();
-    main_box.push_str(&format!("(box :orientation \"{}\" :space-evenly \"false\" :spacing 8 \n", orinentation));
+    main_box.push_str(&format!("(box :orientation \"{}\" :class \"docker-{}-section\" :space-evenly \"false\" :spacing 4 \n", orinentation, name));
 
-    match(name){
-        "name" => main_box.push_str("  (label :class \"docker-container-name\" :text \"Container Name\" :halign \"start\" :valign \"center\")\n"),
-        "image" => main_box.push_str("  (label :class \"docker-container-image\" :text \"Image\" :halign \"start\" :valign \"center\")\n"),
-        "status" => main_box.push_str("  (label :class \"docker-container-status\" :text \"Status\" :halign \"start\" :valign \"center\")\n"),
+    match name {
+        "name" => main_box.push_str("  (label :class \"docker-text-name\" :text \"Container Name\" :halign \"center\" :valign \"center\")\n"),
+        "image" => main_box.push_str("  (label :class \"docker-text-image\" :text \"Image\" :halign \"center\" :valign \"center\")\n"),
+        "status" => main_box.push_str("  (label :class \"docker-text-status\" :text \"Status\" :halign \"center\" :valign \"center\")\n"),
         _ => {}
     }
 
@@ -76,10 +65,10 @@ fn create_section(name: &str, orinentation: &str, list: Vec<String>) -> String{
 fn create_box(name: &str, icon: &str, list: &mut Vec<String>) {
     let mut container = String::new();
 
-    container.push_str(&format!("(box :class \"docker-container-{}\" :orientation \"horizontal\" :space-evenly \"false\" :spacing 15 \n", name));
+    container.push_str(&format!("(box :class \"docker-{}-box\" :orientation \"horizontal\" :space-evenly \"false\" :spacing 10 \n", name));
 
     container.push_str(&format!("  (label :class \"docker-{}-icon\"    :text \"{}\"  :halign \"end\"  :valign \"center\" )\n", name, icon));
-    container.push_str(&format!("  (label :class \"docker-{}\"    :text \"{}\" :halign \"start\"  :valign \"center\" )\n", name, name));
+    container.push_str(&format!("  (label :class \"docker-{}-text\"    :text \"{}\" :halign \"start\"  :valign \"center\" )\n", name, name));
 
     container.push_str(")");  
 
